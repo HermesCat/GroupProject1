93be606d63e133b5323f965da147d3280b748181
HermesCat <jppettis@comcast.net>
2019-03-30 12:48:37 -0400

    merged commit

--- javacript/app.js
+++ javacript/app.js
@@ -1,6 +1,12 @@
 console.log("stuff");
 
+var placeSearch = "richmond";
+var searchResult = `<iframe width="200" height="200" frameborder="0" style="border:0"
+src="https://www.google.com/maps/embed/v1/place?q=${placeSearch}&key=AIzaSyBac2HUKDso4kbbD1NbvLWvtfeWvpdVuWA" allowfullscreen></iframe>`
 
 
+function appendMap() {
+    $("#map").append(searchResult);
+}
 
-
+appendMap();
