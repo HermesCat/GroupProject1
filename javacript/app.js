1d4c81e0c1cf9ac00eb26dd6a79d4790ba862872
Ty Wood <twood06@gmail.com>
2019-03-30 12:34:10 -0400

    changed card and map

--- javacript/app.js
+++ javacript/app.js
@@ -1,15 +1,6 @@
 console.log("stuff");
 
-var queryGoogleAPI = "https://maps.googleapis.com/maps/api/js?key=AIzaSyBbAzKEh32TMIZ6uGIP3SA4RWlCT5V2T7A&callback=initMap"
 
-$.ajax({
-    url: queryGoogleAPI,
-    method: "GET",
-    dataType: 'jsonp',
-    cache: false
-}).then(function (response) {
-    console.log(response);
-});
 
 
 
