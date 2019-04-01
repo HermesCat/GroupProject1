e31fb2e2933b1be980d64ebe0944de305abbefda
Ty Wood <twood06@gmail.com>
2019-04-01 11:07:40 -0400

    tests

--- javacript/app.js
+++ javacript/app.js
@@ -2,17 +2,23 @@
 
 // API KEY
 // var queryURL = "https://newsapi.org/v2/top-headlines?sources=richmondtimesdispatch&apiKey=f14386004b984aab9c45f6dcf17b377f";
- var queryURL = "https://newsapi.org/v2/everything?q=+vcu+richmond&from=2019-03-00&sortBy=relevancy&apiKey=f14386004b984aab9c45f6dcf17b377f";
+var search = $("#search").val().trim();
+var queryURL = `https://newsapi.org/v2/everything?q=${search}&from=2019-03-00&sortBy=relevancy&apiKey=f14386004b984aab9c45f6dcf17b377f`;
 console.log(queryURL);
 
+$("#btn_search").on("click", function () {
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response.articles[3]);
+        
+    });
+    $("#test").prepend(queryURL.articles[3]);
+});
 
 // API CALL
-$.ajax({
-  url: queryURL,
-  method: "GET"
-}).then(function(response) {
-  console.log(response);
-}); 
+
 
 // var req = new Request(queryURL);
 // fetch(req).then(function(response) {
