19b81be04211846bb816d31ae219e9c0a000f8ce
HermesCat <jppettis@comcast.net>
2019-03-31 22:46:43 -0400

    created a prototype html page, it is in its own html file. We can tinker with it a bit before renaming it to 'index'

--- javacript/app.js
+++ javacript/app.js
@@ -21,12 +21,12 @@ $.ajax({
 console.log("stuff");
 
 var placeSearch = "richmond";
-var searchResult = `<iframe width="200" height="200" frameborder="0" style="border:0"
+var searchResult = `<iframe width="95%" height="200" frameborder="0" style="border:0"
 src="https://www.google.com/maps/embed/v1/place?q=${placeSearch}&key=AIzaSyBac2HUKDso4kbbD1NbvLWvtfeWvpdVuWA" allowfullscreen></iframe>`
 
 
 function appendMap() {
-    $("#map").append(searchResult);
-}
+$("#map").append(searchResult);
+  }
 
 appendMap();
